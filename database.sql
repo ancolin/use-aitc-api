847327e39a32b6e081ba544df4a8f29b472c03ff
ancolin <anco.donuts@gmail.com>
2018-04-29 10:16:58 +0900

    update

--- database.sql
+++ database.sql
@@ -16,14 +16,14 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 --
--- Name: plpgsql; Type: EXTENSION; Schema: -; Owner:
+-- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
 --
 
 CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
 
 
 --
--- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner:
+-- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
 --
 
 COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
@@ -38,7 +38,7 @@ SET default_with_oids = false;
 --
 
 CREATE TABLE public.d_xml (
-    path text NOT NULL,
+    filepath text NOT NULL,
     xml xml
 );
 
@@ -46,11 +46,84 @@ CREATE TABLE public.d_xml (
 ALTER TABLE public.d_xml OWNER TO postgres;
 
 --
+-- Name: d_xml_data_advisory; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.d_xml_data_advisory (
+    filepath text NOT NULL,
+    target_area_code text NOT NULL
+);
+
+
+ALTER TABLE public.d_xml_data_advisory OWNER TO postgres;
+
+--
+-- Name: d_xml_header; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.d_xml_header (
+    filepath text NOT NULL,
+    control_title text,
+    head_infokind text,
+    report_date_time timestamp without time zone
+);
+
+
+ALTER TABLE public.d_xml_header OWNER TO postgres;
+
+--
+-- Name: m_city_code; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.m_city_code (
+    pref_code text,
+    area_code text,
+    city_code text NOT NULL,
+    pref_name text,
+    area_name text,
+    city_name text
+);
+
+
+ALTER TABLE public.m_city_code OWNER TO postgres;
+
+--
+-- Name: d_xml_data_advisory d_xml_data_advisory_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.d_xml_data_advisory
+    ADD CONSTRAINT d_xml_data_advisory_pkey PRIMARY KEY (filepath, target_area_code);
+
+
+--
+-- Name: d_xml_header d_xml_header_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.d_xml_header
+    ADD CONSTRAINT d_xml_header_pkey PRIMARY KEY (filepath);
+
+
+--
 -- Name: d_xml d_xml_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
 ALTER TABLE ONLY public.d_xml
-    ADD CONSTRAINT d_xml_pkey PRIMARY KEY (path);
+    ADD CONSTRAINT d_xml_pkey PRIMARY KEY (filepath);
+
+
+--
+-- Name: m_city_code m_city_code_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.m_city_code
+    ADD CONSTRAINT m_city_code_pkey PRIMARY KEY (city_code);
+
+
+--
+-- Name: idx_d_xml_header; Type: INDEX; Schema: public; Owner: postgres
+--
+
+CREATE INDEX idx_d_xml_header ON public.d_xml_header USING btree (control_title, head_infokind, report_date_time);
 
 
 --
